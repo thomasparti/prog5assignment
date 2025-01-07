1c007ae1fc734abb397025dc240a8766f018f32e
thomasparti <107281467+thomasparti@users.noreply.github.com>
2025-01-07 19:25:12 +0100

    Project setup

---
+++ src/main/java/school/prog5assignment/Prog5assignmentApplication.java
@@ -0,0 +1,13 @@
+package school.prog5assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog5assignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog5assignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/prog5assignment/Prog5assignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package school.prog5assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog5assignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
